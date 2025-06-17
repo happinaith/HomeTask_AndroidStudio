5aefb7429415aa7f55bf9be412f422e9f17363b0
Julian <90930614+happinaith@users.noreply.github.com>
2025-06-17 17:17:06 +0300

    Создание приложения по трекингу привычек.

--- app/src/main/java/ru/ulian/helloandroid/MainActivity.kt
+++ app/src/main/java/ru/ulian/helloandroid/MainActivity.kt
@@ -1,20 +1,48 @@
 package ru.ulian.helloandroid
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val firstHabitText = findViewById<TextView>(R.id.FirstHabit)
+        val btnIncrease = findViewById<Button>(R.id.completedButton)
+        val btnReset = findViewById<Button>(R.id.resetButton)
+
+        counter = savedInstanceState?.getInt("counter") ?: 0
+        updateCounterText(firstHabitText)
+
+        btnIncrease.setOnClickListener()
+        {
+            counter++
+            updateCounterText(firstHabitText)
+        }
+        btnReset.setOnClickListener()
+        {
+            counter = 0
+            updateCounterText(firstHabitText)
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("counter", counter)
+    }
+
+
+    private fun updateCounterText(textView: TextView)
+    {
+        textView.text = "Ложиться рано спать: $counter"
+    }
+
 }
